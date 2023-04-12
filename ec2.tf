3d868bd061b61852734ca0aeb7a3f6bd909c2aae
Soumya220 <80702074+Soumya220@users.noreply.github.com>
2023-04-12 17:27:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,8 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami             = ami-07d3a50bd29811cd1
-  instance_type   = t2.micro
-  
-  count           = 1
+  ami             = "ami-07d3a50bd29811cd1"
+  instance_type   = "t2.micro"
 }
