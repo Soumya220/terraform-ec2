029fd4ed23b7da2992970ac0f32c626f06ec58af
Soumya220 <80702074+Soumya220@users.noreply.github.com>
2023-04-12 17:25:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,10 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_instance" "web" {
   ami             = ami-07d3a50bd29811cd1
   instance_type   = t2.micro
+  
   count           = 1
 }
